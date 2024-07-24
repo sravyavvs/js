f3172c9cac4cf1d6b0e5c6748b319ef4e548e612
sravyavvs <45485485+sravyavvs@users.noreply.github.com>
2024-07-24 19:44:10 +0000

    math and num

---
+++ .devcontainer/basics/nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumer = 34.554
+//console.log(otherNumer.toPrecision(3));
+
+///++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+console.log(Math)
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
